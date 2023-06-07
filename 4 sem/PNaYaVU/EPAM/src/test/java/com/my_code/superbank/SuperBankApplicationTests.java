31f81c9eeed360a990f0147bde14f67f8f19052c
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2023-06-07 17:19:18 +0300

    Add files via upload

---
+++ 4 sem/PNaYaVU/EPAM/src/test/java/com/my_code/superbank/SuperBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_code.superbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
