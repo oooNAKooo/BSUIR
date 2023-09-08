0767fc5aeb8bee769d87a747bb47b5aedc5e3d1e
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2023-09-08 12:44:36 +0300

    Update lab1.scala

--- 5 sem/FProgr/lab_1/lab1.scala
+++ 5 sem/FProgr/lab_1/lab1.scala
@@ -20,12 +20,12 @@ def substringFromSymbolsAtNotEvenPositions(str: String): String =
   result.toString()
 }
 
-object Main234 
+object Main1 
 {
 //Unit==void
   def main(args: Array[String]): Unit = 
   {
-    print("Enter the number: ")
+    print("Введите 10-значное число: ")
     var numberString = "" //ввод с клавы строки чисел
     var string_a = "" //1-число(четное)
     var string_b = "" //2-число(нечетное)
@@ -35,25 +35,25 @@ object Main234
     numberString = scala.io.StdIn.readLine() // ввод с консоли
     if((numberString.length() < 10)) 
     {
-      println("Incorrent number")
+      println("Число не 10-значное")
       sys.exit(0)
     }
     for(i <- 1 to 5) //for(int i=1; i<5; i++)
     {
       string_a = substringFromSymbolsAtEvenPositions(numberString)
       string_b = substringFromSymbolsAtNotEvenPositions(numberString)
+      string_a = string_a.substring(0, 5)
+      string_b = string_b.substring(0, 5)
       string_c = string_a.toInt * string_b.toInt //стринг в инт 
       rez = string_c.toString().substring(0,3).toFloat / 1000
       z+=string_c
-      println("a = " + string_a)
-      println("b = " + string_b)
-      println("c = " + string_c)
-      println("rez = " + rez)
-      println("z = " + z)
+      println("Четные позиции = " + string_a)
+      println("Нечетные позиции = " + string_b)
+      println("Произведение = " + string_c)
+      println("Результат = " + rez)
+      println("Сложенное произведение = " + z)
       numberString = numberString.substring(1,numberString.length()-1) 
       numberString = "0" + z //добавляем в начло, чтобы не было -
     }
   }
 }
-
-//
